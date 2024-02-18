992802059e66d1d7c23c51086f8450f39aed6e9b
jonathan hamilton <longjonns@gmail.com>
2024-02-18 11:56:30 +0000

    basic todo list

--- main.go
+++ main.go
@@ -1,22 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 
+type Task struct {
+    Task string `json:"task"`
+}
+
+var tasks []Task = []Task{}
 
 func main() {
 	const port = ":3000"
 	server := http.NewServeMux()
 
 	server.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world!"))
+		jsonResponse, err := json.Marshal(tasks)
+		if err != nil {
+			http.Error(w, "Failed to marshal tasks to JSON", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonResponse)
 	})
 
-	server.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world! from POST"))
+	server.HandleFunc("POST /add", func(w http.ResponseWriter, r *http.Request) {
+		var requestBody Task
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&requestBody); err != nil {
+			http.Error(w, "Failed to decode JSON", http.StatusBadRequest)
+			return
+		}
+
+		tasks = append(tasks, requestBody)
+
+		jsonResponse, err := json.Marshal(tasks)
+		if err != nil {
+			http.Error(w, "Failed to marshal task to JSON", http.StatusInternalServerError)
+			return
+		}
+
+		// Set content type header
+		w.Header().Set("Content-Type", "application/json")
+
+		// Write JSON response
+		w.WriteHeader(http.StatusCreated)
+		w.Write(jsonResponse)
 	})
 
 	fmt.Printf("Server is running at %s\n", port)
